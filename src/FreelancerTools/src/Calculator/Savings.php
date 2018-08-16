09d955a60f52c245c06fa8d8d4fe815abe18675e
Matthew Setter <matthew@matthewsetter.com>
2018-08-16 21:54:54 +0200

    Add function documentation
    Whether this is strictly necessary or not, I'm not sure. But I'm so used
    to doing it that I feel that it is. I felt it also necessary to add some
    of the comments, if for no other reason than to explain fully my
    thinking at the time.

--- src/FreelancerTools/src/Calculator/Savings.php
+++ src/FreelancerTools/src/Calculator/Savings.php
@@ -7,12 +7,31 @@ use \FreelancerTools\Entity\Savings as SavingsEntity;
 
 class Savings
 {
-	public function calculatePercentage(float $paymentAmount, $percentage): float
+	/**
+	 * Calculate a percentage of the payment amount
+	 *
+	 * This is a multi-purpose utility function for calculating a percentage.
+	 * I've decided not to write specific functions for the three values, because
+	 * this one does the job, at the expense of a token amount of readibility.
+	 *
+	 * @param float $paymentAmount
+	 * @param float $percentage
+	 * @return float
+	 */
+	public function calculatePercentage(float $paymentAmount, float $percentage): float
 	{
 		return $paymentAmount * $percentage;
 	}
 
-	public function calculateSavings($payment, $vat, $incomeTax, $savings)
+	/**
+	 * Calculate the amounts to set aside, with or without a VAT applied
+	 *
+	 * @param float $payment
+	 * @param float $vatPerc
+	 * @param float $incomeTaxPerc
+	 * @param float $savingsPerc
+	 * @return SavingsEntity
+	 */
 	public function calculateSavings(float $payment, float $vatPerc, float $incomeTaxPerc, float $savingsPerc): SavingsEntity
 	{
 		$vatAmount = ($vatPerc === 0) ? $vatPerc : $this->calculatePercentage($payment, $vatPerc);
